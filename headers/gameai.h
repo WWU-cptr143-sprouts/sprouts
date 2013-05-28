33aa9bc3cabbdb29d3f970d8be88561ede316805
dullfire <dullfire@yahoo.com>
2013-05-27 20:25:10 -0700

    fixed location of main-ai.cpp

--- headers/gameai.h
+++ headers/gameai.h
@@ -1,1 +1,25 @@
+#ifndef H_Gameai
+#include "headers/game.h"
 
+
+class Pmove
+{
+    public:
+    Connection aLink;
+    Node newNode;
+    Connection bLink;
+};
+
+class GameAI : public Game
+{
+    protected:
+        bool aiFirst, evenINodes;
+        int startingNodes;
+        vector<Pmoves> possibleMoves;
+    public:
+        GameAI();
+        int willMakeNEArea(const Node&,const Node&,const Connection&,const Connection&); //check if a given move will result in a non empty new area;
+
+        ~GameAI();
+};
+#endif
