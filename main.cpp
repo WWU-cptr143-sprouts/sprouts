c242cbacdb85a14d8a5714da9ecaca7d757f643c
Michael Moores <mastersmoores@gmail.com>
2013-06-07 17:39:50 -0700

    "q" key now resets the game and returns to the main menu

--- main.cpp
+++ main.cpp
@@ -86,6 +86,7 @@ int main(int argc, char *argv[])
                         // Q returns to menu
                         else if (event.key.keysym.sym == SDLK_q && !inMenu)
                         {
+                            game = GameGUI(screen);
                             inMenu = true;
                             menu.init();
                         }
