ca002f09f52da6be92c8ea69a51b016bd1390b4e
Michael Moores <mastersmoores@gmail.com>
2013-06-09 12:58:47 -0700

    sited the source of icon2

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int main(int argc, char *argv[])
 
     // Window title and icon
     Image icon("images/icon1.bmp", true);
-    //Image icon("images/icon2.bmp", true); // a second possible icon
+    //Image icon("images/icon2.bmp", true); // a second possible icon found at http://www.merchantcircle.com/static/imgs/answers_homeTopicThumb.png
     SDL_WM_SetIcon(icon.surface(), NULL);
     SDL_WM_SetCaption("Sprouts", "Sprouts");
 
