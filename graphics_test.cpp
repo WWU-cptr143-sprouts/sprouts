f404c12adcfb39475db5050b42aef553d3759e7a
colfor <collin.fornos@wallawalla.edu>
2014-05-14 18:05:30 -0700

    change button color

--- graphics_test.cpp
+++ graphics_test.cpp
@@ -15,12 +15,40 @@ int main()
     vector<function<void()>> needRunFunctions;
     shared_ptr<GUIContainer> gui = make_shared<GUIContainer>(-Display::scaleX(), Display::scaleX(), -Display::scaleY(), Display::scaleY());
     shared_ptr<GUILabel> lastButtonLabel = make_shared<GUILabel>(L"", -0.5, 0.5, -0.15, -0.05);
-    gui->add(make_shared<GUIButton>([lastButtonLabel](){lastButtonLabel->text = L"button 1 pressed"; lastButtonLabel->textColor = Color::RGB(1, 0, 0);}, L"Button 1", -0.4, 0.4, 0.9, 1));
-    gui->add(make_shared<GUIButton>([lastButtonLabel](){lastButtonLabel->text = L"button 2 pressed"; lastButtonLabel->textColor = Color::RGB(1, 1, 0);}, L"Button 2", -0.4, 0.4, 0.7, 0.8));
+    gui->add(make_shared<GUIButton>([lastButtonLabel]()
+    {
+        lastButtonLabel->text = L"button 1 pressed";
+        lastButtonLabel->textColor = Color::RGB(1, 0, 0);
+    }, L"Button 1", -0.4, 0.4, 0.9, 1));
+    gui->add(make_shared<GUIButton>([lastButtonLabel]()
+    {
+        lastButtonLabel->text = L"button 2 pressed";
+        lastButtonLabel->textColor = Color::RGB(1, 1, 0);
+    }, L"Button 2", -0.4, 0.4, 0.7, 0.8));
     shared_ptr<GUILabel> fpsLabel = make_shared<GUILabel>(L"", -0.5, 0.5, -0.05, 0.05);
     gui->add(make_shared<GUIContainer>(-0.5, 0.5, -0.4, 0.1)->add(fpsLabel)->add(lastButtonLabel));
-    gui->add(make_shared<GUIButton>([&needRunFunctions, &gui](){needRunFunctions.push_back([&gui](){endGraphics(); setPreferredMode(640, 480); startGraphics(); Display::initFrame(); gui->reset();});}, L"Set 640x480 graphics", -0.4, 0.4, 0.3, 0.4));
-    gui->add(make_shared<GUIButton>([&needRunFunctions, &gui](){needRunFunctions.push_back([&gui](){endGraphics(); setPreferredMode(1024, 768); startGraphics(); Display::initFrame(); gui->reset();});}, L"Set 1024x768 graphics", -0.4, 0.4, -0.4, -0.3));
+    gui->add(make_shared<GUIButton>([&needRunFunctions, &gui]()
+    {
+        needRunFunctions.push_back([&gui]()
+        {
+            endGraphics();
+            setPreferredMode(640, 480);
+            startGraphics();
+            Display::initFrame();
+            gui->reset();
+        });
+    }, L"Set 640x480 graphics", -0.4, 0.4, 0.3, 0.4));
+    gui->add(make_shared<GUIButton>([&needRunFunctions, &gui]()
+    {
+        needRunFunctions.push_back([&gui]()
+        {
+            endGraphics();
+            setPreferredMode(1024, 768);
+            startGraphics();
+            Display::initFrame();
+            gui->reset();
+        });
+    }, L"Set 1024x768 graphics", -0.4, 0.4, -0.4, -0.3, Color::RGB(1, 0, 0), Color::V(0), Color::RGB(0, 0, 1)));
     while(true)
     {
         Display::initFrame();
